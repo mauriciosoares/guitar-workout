aa4e116fb9cd1effbc913f892aeb4f087406aebe
Mauricio Soares <m.soares2603@gmail.com>
2019-07-01 00:34:10 +0200

    gh-pages hack to have SPA and pushState

--- src/shared/paths.js
+++ src/shared/paths.js
@@ -1,3 +1,3 @@
-export const WORKOUT = "/workout/:id";
-export const POPULAR = "/popular";
-export const STARRED = "/starred";
+export const WORKOUT = "/guitar-workout/workout/:id";
+export const POPULAR = "/guitar-workout/popular";
+export const STARRED = "/guitar-workout/starred";
