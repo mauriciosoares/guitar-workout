013cf2e2919d1949fa8f725a6f0a38fec1f341a9
Mauricio Soares <m.soares2603@gmail.com>
2019-07-01 00:09:49 +0200

    Updates

---
+++ precache-manifest.f3f950a5853a3cd73b7bc65421532eb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77e436916c5fb77adac33ea959f4aab",
+    "url": "/guitar-workout/index.html"
+  },
+  {
+    "revision": "e4d3b8c592b15d288e9e",
+    "url": "/guitar-workout/static/css/2.33bc3cad.chunk.css"
+  },
+  {
+    "revision": "1603676651e8341d2932",
+    "url": "/guitar-workout/static/css/main.d259fb41.chunk.css"
+  },
+  {
+    "revision": "e4d3b8c592b15d288e9e",
+    "url": "/guitar-workout/static/js/2.3c3490e6.chunk.js"
+  },
+  {
+    "revision": "1603676651e8341d2932",
+    "url": "/guitar-workout/static/js/main.8e7999cf.chunk.js"
+  },
+  {
+    "revision": "8339ed927ba61544258e",
+    "url": "/guitar-workout/static/js/runtime~main.05968900.js"
+  },
+  {
+    "revision": "106011f43733abe6dbb2dd8c2b1ea396",
+    "url": "/guitar-workout/static/media/beep-primary.106011f4.mp3"
+  },
+  {
+    "revision": "272df74fff2d7ad1125848fd057b6fbb",
+    "url": "/guitar-workout/static/media/beep.272df74f.mp3"
+  }
+]);
